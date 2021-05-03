aec46b031bbeb8e6e39e35401d875ab22403349d
Mohit Kalra <Mohit.Kalra@GOINDIGO.IN>
2021-05-03 21:01:21 +0530

    temp version 3

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -8,13 +8,8 @@ import Link from 'next/link'
 export default function Logo({ ...props }: BoxProps) {
   return (
     <Box {...props} >
-<<<<<<< HEAD
       <div className="logo-img"></div>
       <div className="log-text" style={{ width: 350 }}>
-=======
-      <div ></div>
-      <div  style={{ width: 350 }}>
->>>>>>> deb8d8f048b7c8a364632b0a69a58b1df9e45406
         <Text paddingLeft="60px" paddingTop="5px" color="black" fontSize="md" fontWeight="bold">
           <Link href="/">My Covid Time</Link>
         </Text>
