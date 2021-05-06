dcbe5fa2dcef9f1a15d3ebcb43be795af73834ff
kalramohit <kalra.mohit@gmail.com>
2021-05-06 15:14:26 +0530

    Update new.tsx
    story submission page change

--- pages/new.tsx
+++ pages/new.tsx
@@ -21,7 +21,6 @@ export default function CreateStory() {
         </Heading>
         <Text>
            Let the world hear about YOUR own experience of this COVID-19 pandemic. Over the past weeks, maybe months, you have been taking action and developing many creative ideas to face these challenging times, whether by helping your community, finding innovative learning ways, keeping a positive spirit, taking care of your relatives and loved ones, and much much more.
-          
         </Text>
         <Text>
           <br />
@@ -30,6 +29,12 @@ export default function CreateStory() {
         <Text fontWeight="bold" >
         <br />
           This is an open invitation to all of you to share your stories or stories known to you and inspire us all. We will publish your story after doing validation and formatting.
+        </Text>
+        <Text>
+          <br />
+          You may submit your story over the email or filling up below form. In case if you would like to share images along with your story, please share the story over the email.
+          <br />
+          You can email the story at this address <a cssClass="chakra-link css-1gr1s6a" href="mailto:info@mycovidtime.in">info@mycovidtime.in</a>
         </Text>
         <Text pt={4} pb={2} >
         Please read our <span style={{fontWeight:"bold"}}><Link  href="/faq">FAQ</Link></span> for validation process.
