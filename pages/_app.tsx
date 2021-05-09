c8b695c6679c65c7f108e1aa43bbc1509af7c3b1
kalramohit <kalra.mohit@gmail.com>
2021-05-09 16:37:09 +0530

    Update _app.tsx
    icons added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -56,7 +56,7 @@ function MyApp({ Component, pageProps }: MyAppProps) {
     sameAs: [
       'https://www.facebook.com/MyCovidTime',
       'https://twitter.com/MyCOVIDTime',
-      'https://www.instagram.com/MyCovidTijme/',
+      'https://www.instagram.com/MyCovidTime/',
       'https://github.com/kalramohit/my-covid-time/',
       'https://www.MyCovidTime.in',
     ],
@@ -69,12 +69,23 @@ function MyApp({ Component, pageProps }: MyAppProps) {
         <meta key="twitter:card" name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@MyCOVIDTime" />
         <meta property="og:type" content="website" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
         <link rel="manifest" href="/site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
